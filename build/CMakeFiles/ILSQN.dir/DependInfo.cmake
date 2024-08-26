550a46e5cc18a76f908874c56a0fcc294f788790
klaoli <1462893983@qq.com>
2024-08-26 14:27:17 +0800

    commit

--- build/CMakeFiles/ILSQN.dir/DependInfo.cmake
+++ build/CMakeFiles/ILSQN.dir/DependInfo.cmake
@@ -1,24 +1,25 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/algo/llf/2D_Irregular_Packing/dataloader.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/dataloader.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/datawriter.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/datawriter.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/geometry.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/geometry.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/ilsqn.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/ilsqn.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/include/clipper.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/include/clipper.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/main.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/main.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/nofitpolygon.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/nofitpolygon.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/packing.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/packing.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/parameters.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/parameters.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/piece.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/piece.cpp.o"
+  "/home/algo/llf/2D_Irregular_Packing/vector.cpp" "/home/algo/llf/2D_Irregular_Packing/build/CMakeFiles/ILSQN.dir/vector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/llf/Desktop/ILSQN-New/dataloader.cpp" "CMakeFiles/ILSQN.dir/dataloader.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/dataloader.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/datawriter.cpp" "CMakeFiles/ILSQN.dir/datawriter.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/datawriter.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/geometry.cpp" "CMakeFiles/ILSQN.dir/geometry.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/geometry.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/ilsqn.cpp" "CMakeFiles/ILSQN.dir/ilsqn.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/ilsqn.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/include/clipper.cpp" "CMakeFiles/ILSQN.dir/include/clipper.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/include/clipper.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/main.cpp" "CMakeFiles/ILSQN.dir/main.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/main.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/nofitpolygon.cpp" "CMakeFiles/ILSQN.dir/nofitpolygon.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/nofitpolygon.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/packing.cpp" "CMakeFiles/ILSQN.dir/packing.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/packing.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/parameters.cpp" "CMakeFiles/ILSQN.dir/parameters.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/parameters.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/piece.cpp" "CMakeFiles/ILSQN.dir/piece.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/piece.cpp.o.d"
-  "/home/llf/Desktop/ILSQN-New/vector.cpp" "CMakeFiles/ILSQN.dir/vector.cpp.o" "gcc" "CMakeFiles/ILSQN.dir/vector.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
